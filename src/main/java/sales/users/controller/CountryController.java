11c1f656d95715e48e5df764fd716667c74b8289
Taras Danylyuk <tarik.danylyuk@gmail.com>
2015-08-04 15:42:07 +0000

    modificated users, added address domain

--- src/main/java/sales/users/controller/CountryController.java
+++ src/main/java/sales/users/controller/CountryController.java
@@ -65,6 +65,7 @@ public class CountryController {
             method = RequestMethod.GET,
             value = "/country/{id}/region",
             produces = "application/json")
+
     public List<Region> getAllRegions(@PathVariable("id") Long id) {
         logger.debug("Get list of regions");
         return regionService.getAll(countryService.getOne(id));
